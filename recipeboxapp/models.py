2803337d087dfb4a43ec5a487cb5c0cf5c3d6c29
Erick <ErickSibrian1@gmail.com>
2022-08-21 11:48:58 -0400

    models

--- recipeboxapp/models.py
+++ recipeboxapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+
+class Author(...):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    byline = models.TextField()
+
+
+class Article(...):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    author = models.ForeignKey(...)
+    time_created = models.DateTimeField(timezone.now)
